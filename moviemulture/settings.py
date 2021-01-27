f515f55e35fe09f7ccc4971f6c7392ac34505704
iravikumar <iravikumar92@gmail.com>
2021-01-27 23:37:11 +0530

    Changes in Settings

--- moviemulture/settings.py
+++ moviemulture/settings.py
@@ -75,18 +75,18 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # },
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'moviemulture',
-    },
-#     'default':{
-#         'ENGINE':'django.db.backends.postgresql_psycopg2',
-#         'NAME':'moviemulture',
-#         'USER':'xxxxxxx',
-#         'PASSWORD':'xxxxxxx',
-#         'HOST':'localhost',
-#         'PORT':'5432',
-#    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'moviemulture',
+    # },
+    'default':{
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME':'moviemulture',
+        'USER':'xxxxxxx',
+        'PASSWORD':'xxxxxxx',
+        'HOST':'localhost',
+        'PORT':'5432',
+   }
 }
 
 
